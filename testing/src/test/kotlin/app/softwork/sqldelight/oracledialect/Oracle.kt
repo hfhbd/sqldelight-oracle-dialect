b7141a50edfce7464bfe235dc320c3543e880827
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-11-16 15:00:55 +0100

    Fix fixtures (#3)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- testing/src/test/kotlin/app/softwork/sqldelight/oracledialect/Oracle.kt
+++ testing/src/test/kotlin/app/softwork/sqldelight/oracledialect/Oracle.kt
@@ -3,15 +3,20 @@ package app.softwork.sqldelight.oracledialect
 import app.cash.sqldelight.driver.jdbc.*
 import oracle.jdbc.datasource.impl.*
 import org.testcontainers.containers.*
+import org.testcontainers.utility.*
 
 object Oracle {
     val driver = run {
-        val container = OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
+        val myImage = DockerImageName.parse("gvenzl/oracle-free:23-slim-faststart").asCompatibleSubstituteFor("gvenzl/oracle-xe")
+        val container = OracleContainer(myImage)
         container.start()
         OracleDataSource().apply {
+            driverType = "thin"
+            serverName = "localhost"
+            serviceName = "FREEPDB1"
+            portNumber = container.firstMappedPort
             databaseName = "xepdb1"
             user = "test"
-            portNumber = container.firstMappedPort
             setPassword("test")
         }.asJdbcDriver()
     }
