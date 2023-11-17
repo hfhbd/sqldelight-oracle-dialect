006113e83cd5c6a09c0be3ab65a674bcc451b3ff
hfhbd <hfhbd@users.noreply.github.com>
2023-11-17 12:18:35 +0100

    Add Number data type

--- testing/src/test/kotlin/app/softwork/sqldelight/oracledialect/Testing.kt
+++ testing/src/test/kotlin/app/softwork/sqldelight/oracledialect/Testing.kt
@@ -1,6 +1,7 @@
 package app.softwork.sqldelight.oracledialect
 
 import app.softwork.sqldelight.oracledialect.Oracle.driver
+import java.time.*
 import kotlin.test.*
 
 class Testing {
@@ -10,10 +11,10 @@ class Testing {
         val db = TestingDB(driver)
 
         assertEquals(emptyList(), db.fooQueries.getAll().executeAsList())
-        db.fooQueries.new(Foo(42, "Foo"))
-        assertEquals(listOf(Foo(42, "Foo")), db.fooQueries.getAll().executeAsList())
+        db.fooQueries.new(Foo(42, "Foo", 1.toBigDecimal(), LocalDate.EPOCH))
+        assertEquals(listOf(Foo(42, "Foo", 1.toBigDecimal(), LocalDate.EPOCH)), db.fooQueries.getAll().executeAsList())
 
-        db.fooQueries.create(Foo(100, "Bar"))
-        assertEquals(listOf(Foo(42, "Foo"), Foo(100, "Bar")), db.fooQueries.getAll().executeAsList())
+        db.fooQueries.create(Foo(100, "Bar", 1.toBigDecimal(), LocalDate.EPOCH))
+        assertEquals(listOf(Foo(42, "Foo", 1.toBigDecimal(), LocalDate.EPOCH), Foo(100, "Bar", 1.toBigDecimal(), LocalDate.EPOCH)), db.fooQueries.getAll().executeAsList())
     }
 }
